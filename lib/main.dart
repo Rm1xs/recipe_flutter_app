f3bbe61d422af75e1957e11acc4af1004dcb8a1b
Rmixs <Buggati14>
2021-08-05 21:16:23 +0300

    lang error

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_flutter_app/core/animation/main_lottie_animation.dart';
+import 'package:recipe_flutter_app/core/localization/app_localizations.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
@@ -12,10 +13,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Recipe App',
+      title: AppLocalizations.of(context)!.translate('app_name')!,
       theme: ThemeData(primaryColor: Colors.brown,),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.system,
+      supportedLocales: [
+        Locale('en', 'US'),
+        Locale('ru', 'RU'),
+      ],
+
+      localizationsDelegates: [
+
+        AppLocalizations.delegate,
+
+      ],
+
+      localeResolutionCallback: (locale, supportedLocales) {
+
+        for (var supportedLocale in supportedLocales) {
+          if (supportedLocale.languageCode == locale!.languageCode &&
+              supportedLocale.countryCode == locale.countryCode) {
+            return supportedLocale;
+          }
+        }
+        return supportedLocales.first;
+      },
       home: SafeArea(child: MainLottieAnimation()),
     );
   }
